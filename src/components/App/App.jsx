054e86f03925fb4ae2b5444005bb2994419dc5c6
Jonathon Baxendell <j.bax626@gmail.com>
2022-08-25 20:27:59 -0500

    router setup and get route in app jsx

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,15 +1,37 @@
 import React from 'react';
+import {useState, useEffect} from 'react';
+import axios from 'axios';
 
-import Header from '../Header/Header.jsx'
+import Header from '../Header/Header.jsx';
 import './App.css';
 
 
 function App() {
+
+    const [groceryList, setGroceryList] = useState([]);
+
+    useEffect(() => {
+        console.log('useEffect - page load');
+        fetchGroceryList();
+    }, []);
+
+    const fetchGroceryList = () => {
+        axios({
+            method: 'GET',
+            url: '/grocery'
+        }).then(response => {
+            setGroceryList(response.data);
+        }).catch(error => {
+            console.log(error);
+            alert('Something went wrong!');
+          });
+    }
+
     return (
         <div className="App">
             <Header />
             <main>
-                <p>Under Construction...</p>
+                <p>{JSON.stringify(groceryList)}</p>
             </main>
         </div>
     );
